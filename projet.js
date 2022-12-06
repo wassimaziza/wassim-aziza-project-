a7401c1bf1ba94d2670bd0c86055cdfa99dbe157
wassim aziza <wassimaziza56@gmail.com>
2022-12-06 09:47:24 +0100

    .

---
+++ projet.js
@@ -0,0 +1,31 @@
+function myFunction() {
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "text");
+  x.setAttribute("value", "added to cart ,great choice!");
+  document.body.appendChild(x);
+}
+
+$("#1").hover(function(){
+ $("#1").fadeOut(500)
+ $("#1").fadeIn(500)})
+ $("#2").hover(function(){
+ $("#2").fadeOut(500)
+ $("#2").fadeIn(500)})
+ $("#3").hover(function(){
+ $("#3").fadeOut(500)
+ $("#3").fadeIn(500)})
+ $("#4").hover(function(){
+ $("#4").fadeOut(500)
+ $("#4").fadeIn(500)})
+ $("#5").hover(function(){
+ $("#5").fadeOut(500)
+ $("#5").fadeIn(500)})
+ $("#6").hover(function(){
+ $("#6").fadeOut(500)
+ $("#6").fadeIn(500)})
+ $("#7").hover(function(){
+ $("#7").fadeOut(500)
+ $("#7").fadeIn(500)})
+ $("#8").hover(function(){
+ $("#8").fadeOut(500)
+ $("#8").fadeIn(500)})
